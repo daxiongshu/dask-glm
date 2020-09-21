cccac1edf3ca1a41288c38d440895eb528475826
daxiongshu <aixueer4ever@gmail.com>
2020-09-21 18:00:47 -0400

    try lbfgs

--- dask_glm/algorithms.py
+++ dask_glm/algorithms.py
@@ -11,7 +11,7 @@ import cupy as cp
 import dask.array as da
 from scipy.optimize import fmin_l_bfgs_b
 
-
+from dask.array.utils import normalize_to_array
 from dask_glm.utils import dot, normalize, scatter_array, get_distributed_client
 from dask_glm.families import Logistic
 from dask_glm.regularizers import Regularizer
@@ -329,6 +329,7 @@ def lbfgs(X, y, regularizer=None, lamduh=1.0, max_iter=100, tol=1e-4,
     beta : array-like, shape (n_features,)
     """
     dask_distributed_client = get_distributed_client()
+    #print('dask_distributed_client', dask_distributed_client)
     pointwise_loss = family.pointwise_loss
     pointwise_gradient = family.pointwise_gradient
     if regularizer is not None:
@@ -338,22 +339,27 @@ def lbfgs(X, y, regularizer=None, lamduh=1.0, max_iter=100, tol=1e-4,
 
     n, p = X.shape
     beta0 = cp.zeros(p)
-
+    #print(X)
+    #print(y)
     def compute_loss_grad(beta, X, y):
         scatter_beta = scatter_array(
             beta, dask_distributed_client) if dask_distributed_client else beta
-        print(type(X), type(y), type(scatter_beta))
+        #print(type(X), type(y), type(scatter_beta))
+        #scatter_beta = cp.asarray(beta)
+        print(scatter_beta, X, y)
         loss_fn = pointwise_loss(scatter_beta, X, y)
         gradient_fn = pointwise_gradient(scatter_beta, X, y)
         loss, gradient = compute(loss_fn, gradient_fn)
-        return loss, gradient.copy()
+        #print(loss)
+        #print(gradient)
+        return normalize_to_array(loss), normalize_to_array(gradient.copy())
 
     with dask.config.set(fuse_ave_width=0):  # optimizations slows this down
         beta, loss, info = fmin_l_bfgs_b(
-            compute_loss_grad, beta0, fprime=None,
+            compute_loss_grad, normalize_to_array(beta0), fprime=None,
             args=(X, y),
             iprint=(verbose > 0) - 1, pgtol=tol, maxiter=max_iter)
-
+    print('algo done')
     return beta
 
 
